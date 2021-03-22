7079b52a4a4a7f80d8eb8c2bd4743f7d4ef5759e
Elio <netoo.elio@hotmail.com>
2021-03-22 15:34:34 -0300

    github link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ export default function Footer(){
     <Navbar bg='success' variant='dark' sticky='bottom' className='footer text-center flex'>
       <Navbar.Brand href="#home">
         <Row>
-          <Col className='flex-item-1 footer-item'><Button variant="outline-light" href='#' ><FaGithubAlt/></Button></Col>
+          <Col className='flex-item-1 footer-item'><Button variant="outline-light" href='https://github.com/ElioNeto/refactor-ts' ><FaGithubAlt/></Button></Col>
           <Col className='flex-item-1 footer-item'><b>Desenvolvido por: </b>Elio Neto</Col>
           <Col className='flex-item-1 footer-item' ><small>2021</small></Col>
         </Row>
